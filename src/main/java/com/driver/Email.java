6b9a235ea367b747d77ead431eda87221c756ae0
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-04-03 14:13:38 +0530

    changes 3

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -6,7 +6,7 @@ public class Email {
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -19,30 +19,32 @@ public class Email {
         return password;
     }
 
-    public boolean isvalidPassword(String password){
+    public boolean isvalidPassword(String password) {
         boolean upperCase = false;
-        boolean lowerCase= false;
-        boolean specialChar= false;
-        boolean digit= false;
-        if(password.length() < 8)
-            return false;
-        for(int i=0; i<password.length(); i++){
-           char ch = password.charAt(i);
-           if(Character.isUpperCase(ch))
-               upperCase = true;
-            if(Character.isLowerCase(ch))
+        boolean lowerCase = false;
+        boolean specialChar = false;
+        boolean digit = false;
+//        if (password.length() < 8)
+//            return false;
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if (Character.isUpperCase(ch))
+                upperCase = true;
+            if (Character.isLowerCase(ch))
                 lowerCase = true;
-            if(Character.isDigit(ch))
+            if (Character.isDigit(ch))
                 digit = true;
-            if(!Character.isLetter(ch) && !Character.isDigit(ch) && !Character.isWhitespace(ch))
+            if (!Character.isLetter(ch) && !Character.isDigit(ch) && !Character.isWhitespace(ch))
                 specialChar = true;
         }
-        if(upperCase && lowerCase && digit && specialChar)
+        if (upperCase && lowerCase && digit && specialChar)
             return true;
+
         return false;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
@@ -50,16 +52,12 @@ public class Email {
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
 
-        if(oldPassword == this.password) {
-            if(isvalidPassword(newPassword)){
+        if (oldPassword == this.password) {
+            if (newPassword.length() >= 8 && isvalidPassword(newPassword)) {
                 this.password = newPassword;
+            } else {
+                System.out.println("Recent Password does not match with old password ");
             }
-            else{
-                System.out.println("Enter a valid password");
-            }
-        }
-        else{
-            System.out.println("Recent Password does not match with old password ");
         }
     }
 }
